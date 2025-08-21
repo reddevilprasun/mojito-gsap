6333378f21b707cc1765fe94d691813ffb6c641e
reddevilprasun <prasunkamilya26@gmail.com>
2025-08-21 17:36:42 +0530

    add Navbar component and update index.html with new title and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
-
+      <Navbar />
     </main>
   )
 }
