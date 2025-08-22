e51cf96496c7b0181e30b84beca142cb1b0da751
reddevilprasun <prasunkamilya26@gmail.com>
2025-08-22 09:58:05 +0530

    Implement About Section

---
+++ src/components/About.jsx
@@ -0,0 +1,128 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import React from "react";
+
+const About = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#about h2", {
+      type: "words",
+    });
+
+    const subContentSplit = SplitText.create("#about .sub-content p", {
+      type: "lines",
+    });
+
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+      },
+    });
+
+    scrollTimeline
+      .from(titleSplit.words, {
+        yPercent: 100,
+        stagger: 0.03,
+        duration: 1.5,
+        opacity: 0,
+        ease: "expo.out",
+      })
+      .from(
+        subContentSplit.lines,
+        {
+          yPercent: 100,
+          opacity: 0,
+          stagger: 0.04,
+          duration: 1,
+          ease: "expo.out",
+        },
+        "-=1"
+      )
+      .from(
+        ".top-grid div, .bottom-grid div",
+        {
+          opacity: 0,
+          duration: 1,
+          ease: "power1.inOut",
+          stagger: 0.04,
+        },
+        "-=0.5"
+      );
+  });
+  return (
+    <div id="about">
+      <div className=" mb-16 md:px-0 px-5">
+        <div className="content">
+          <div className="md:col-span-8">
+            <p className="badge">Best Cocktails</p>
+            <h2>
+              Where every detail matters <span className=" text-white">-</span>
+              from muddle to garnish
+            </h2>
+          </div>
+          <div className="sub-content">
+            <p>
+              Every cocktail we serve is a reflection of our obsession with
+              detail — from the first muddle to the final garnish. That care is
+              what turns a simple drink into something truly memorable.
+            </p>
+            <div>
+              <div className="flex flex-row justify-start space-x-1">
+                {[...Array(5)].map((_, index) => (
+                  <svg
+                    key={index}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="white"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2.5}
+                    stroke="currentColor"
+                    className="w-7 h-7"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 17.27l5.18 3.73-1.64-6.36L20 9.24l-6.4-.52L12 3 10.4 8.72 4 9.24l4.46 5.4-1.64 6.36L12 17.27z"
+                    />
+                  </svg>
+                ))}
+              </div>
+              <p className="md:text-3xl text-xl font-bold">
+                <span>4.5</span>/5
+              </p>
+              <p className="text-sm text-white-100">
+                More than +12000 customers
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="top-grid">
+        <div className="md:col-span-3">
+          <div className="noisy" />
+          <img src="/images/abt1.png" alt="grid-img-1" />
+        </div>
+        <div className="md:col-span-6">
+          <div className="noisy" />
+          <img src="/images/abt2.png" alt="grid-img-2" />
+        </div>
+        <div className="md:col-span-3">
+          <div className="noisy" />
+          <img src="/images/abt5.png" alt="grid-img-5" />
+        </div>
+      </div>
+      <div className="bottom-grid">
+        <div className="md:col-span-8">
+          <div className="noisy" />
+          <img src="/images/abt3.png" alt="grid-img-3" />
+        </div>
+        <div className="md:col-span-4">
+          <div className="noisy" />
+          <img src="/images/abt4.png" alt="grid-img-4" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
